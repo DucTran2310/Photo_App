8f818c2ebbc3c6dd4224d89464c7c57d28d7900a
Adstar <18520021@gm.uit.edu.vn>
2021-08-24 15:58:44 +0700

    Update everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+
+// Lazy load - Code splitting
+const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="photo-app">
+            <Suspense fallback={<div>Loading ...</div>}>
+                <BrowserRouter>
+                    <Header />
+
+                    <Switch>
+                        <Redirect exact from="/" to="/photos" />
+
+                        <Route path="/photos" component={Photo} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;
