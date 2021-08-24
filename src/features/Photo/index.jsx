2cbbe0c2dbde4f57b2e71d9bcd6ecfb45c87087d
Adstar <18520021@gm.uit.edu.vn>
2021-08-24 21:14:18 +0700

    Edit path

--- src/features/Photo/index.jsx
+++ src/features/Photo/index.jsx
@@ -1,12 +1,12 @@
+import NotFound from 'components/NotFound';
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import NotFound from '../../components/NotFound';
 import AddEditPage from './pages/AddEdit';
 import MainPage from './pages/Main';
 
 Photo.propTypes = {};
 
-function Photo(props) {
+function Photo() {
     const match = useRouteMatch();
     console.log({ match });
 
